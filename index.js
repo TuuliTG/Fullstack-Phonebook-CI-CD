3a66cc59349f6f02e3d79e69b5a8e5c097b1229d
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2023-08-01 22:13:50 +0300

    Add linter support for cypress

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-
-//const morgan = require('morgan')
-
-//morgan.token('content', (request) => {
-//  return JSON.stringify(request.body)
-//})
-
-//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
